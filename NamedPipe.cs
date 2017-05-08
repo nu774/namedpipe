2d74c1d5ee9dbdd1ac66c46ea4eed7ad6964f6b0
nu774 <honeycomb77@gmail.com>
2017-05-08 22:01:24 +0900

    initial

---
+++ NamedPipe.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+
+namespace NamedPipe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 0)
+                    throw new ArgumentException("usage: namedpipe PIPENAME");
+
+                var pipeName = args[0];
+                if (pipeName.Replace('/', '\\').ToLower().StartsWith(@"\\.\pipe\"))
+                    pipeName = pipeName.Substring(9);
+                using (var stream = new NamedPipeServerStream(pipeName, PipeDirection.InOut, 1, PipeTransmissionMode.Byte, PipeOptions.Asynchronous))
+                {
+                    stream.WaitForConnection();
+                    Task.Run(async () =>
+                    {
+                        await Console.OpenStandardInput().CopyToAsync(stream);
+                        stream.Close();
+                    });
+                    stream.CopyToAsync(Console.OpenStandardOutput()).Wait();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+                Environment.Exit(2);
+            }
+        }
+    }
+}
---
+++ README.rst
@@ -0,0 +1,2 @@
+Creates a server-side named pipe, and relays between stdin/stdout and pipe bidirectionally.
+Requires .NET framework >= 4.5.
